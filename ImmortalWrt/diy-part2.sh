628d64424025abc8f070185ffd6a42fc4b14a950
Victor <fy123nig@gmail.com>
2025-01-17 13:18:56 +0800

    Update diy-part2.sh

--- ImmortalWrt/diy-part2.sh
+++ ImmortalWrt/diy-part2.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/Bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/Bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
+# sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
+
+
+# Modify hostname
+# sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/content="${explain}${header}"/content="${content}${explain}${header}"/g' ./feeds/luci/applications/luci-app-wechatpush/root/usr/share/wechatpush/wechatpush
